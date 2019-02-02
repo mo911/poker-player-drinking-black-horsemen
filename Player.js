364f0c5b869f579db15ed0b0e50f2e6297984ba9
Mo <adammolnar.dev@innonic.com>
2019-02-02 10:38:19 +0100

    highcards

--- Player.js
+++ Player.js
@@ -4,14 +4,22 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
+    let highCards = ['A','K','Q','J'];
+
     console.error(gameState.players[2].hole_cards);
     if(gameState.players[2].hole_cards[0].rank == gameState.players[2].hole_cards[1].rank){
       bet(gameState.players[2].stack);
       console.error("megad");
-    }else{
-      console.error("dob");
-      //bet(gameState.players[2].stack);
-      bet(0);
+    }
+    else{
+      if(highCards.includes(gameState.players[2].hole_cards[0].rank) && highCards.includes(gameState.players[2].hole_cards[1].rank)){
+        bet(gameState.players[2].stack);
+        console.error("megad");
+      }else{
+        console.error("dob");
+        //bet(gameState.players[2].stack);
+        bet(0);
+      }
     }
     
   }
