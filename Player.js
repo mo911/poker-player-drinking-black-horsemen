2ec4724b3ee169ea04306d2a8bc26359aacf7330
Mo <adammolnar.dev@innonic.com>
2019-02-02 10:33:29 +0100

    dobmegad

--- Player.js
+++ Player.js
@@ -5,9 +5,15 @@ class Player {
 
   static betRequest(gameState, bet) {
     console.error(gameState.players[2].hole_cards);
-    console.error(gameState);
-    //bet(gameState.players[2].stack);
-    bet(0);
+    if(gameState.players[2].hole_cards[0].rank == gameState.players[2].hole_cards[1].rank){
+      bet(gameState.players[2].stack);
+      console.error("megad");
+    }else{
+      console.error("dob");
+      //bet(gameState.players[2].stack);
+      bet(0);
+    }
+    
   }
 
   static showdown(gameState) {
